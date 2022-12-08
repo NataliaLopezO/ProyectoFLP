f94e9bce7558b2b4191051997734a0cd457833c8
NataliaLopezO <natalia.lopez.osorio@correounivalle.edu.co>
2022-12-08 15:36:53 -0500

    primera parte gramatica

--- InterpretadorMini-PY.rkt
+++ InterpretadorMini-PY.rkt
@@ -3,5 +3,106 @@
 ;;Natalia Lopez Osorio - 2025618
 ;;Carolain JImenez Bedoya - 2071368
 ;;Juan Steban Diaz - 2024147
-;;Gabriel Franco - 2024200
 ;;Hernando Lopez - 2022318
+
+;******************************************************************************************
+
+;; La definición BNF para las expresiones del lenguaje:
+;;
+;;  <programa>     ::= <expresion>
+;;                     <un-programa (exp)>
+
+;;  <expresion>    ::= <numero>
+;;                     <numero-lit  (num)>
+
+;;                 := "\""<texto> "\""
+;;                     <texto-lit (txt)>
+
+;;                 ::= <identificador>
+;;                     <id-exp (id)>
+
+;;                 ::= (<expression><primitiva-binaria><expression>)
+;;                     <primapp-bin-exp (exp1 prim-binaria exp2)>
+
+;;                 ::= <primitiva-unaria>(<expression>)
+;;                     <primapp-un-exp (prim-unaria exp)>
+
+;;                 ::= Si <expresion> entonces <expresion> sino <expression> finSI
+;;                      <condicional-exp (test-exp true-exp false-exp)>
+
+;;                 := procedimiento (<identificador>*',') haga <expresion> finProc
+;;                    <procedimiento-ex (ids cuerpo)>
+
+;;                 := evaluar <expresion>(<expresion> ",")* finEval
+;;                    <app-exp(exp exps)>
+
+;;                 ::= letrec  {identifier ({identifier}*(,)) = <expression>}* in <expression>
+;;                     <letrec-exp proc-names idss bodies bodyletrec>
+
+;;                 ::= var {<identificador> = <expresion> }*(;) in <expresion>
+;;                     <var-exp idsVar expsVar cuerpoVar>
+
+;;                 ::= const {<identificador> = <expresion> }*(;) in <expresion>
+;;                     <const-exp idsConst expsConst cuerpoConst>
+
+
+
+
+
+;******************************************************************************************
+
+;Especificación Léxica
+
+(define scanner-spec-simple-interpreter
+'(
+  (white-sp    (whitespace) skip)
+  (comentario     ("%" (arbno (not #\newline))) skip)
+  (identificador  ("@" letter (arbno (or letter digit))) symbol)
+  (texto        (letter (arbno (or letter digit ":" "?" "=" "'" "#" "$" "&" "." "," ";" "*" "!" "¡" "¿" "-" "_"))) string)
+  (numero       (digit (arbno digit)) number)
+  (numero       ("-" digit (arbno digit)) number)
+  (numero       (digit (arbno digit) "." digit (arbno digit)) number)
+  (numero       ("-" digit (arbno digit) "." digit (arbno digit)) number)
+ )
+)
+
+;Especificación Sintáctica (gramática)
+
+(define grammar-simple-interpreter
+  '(
+    ;;Programa
+    
+    (programa (expresion) un-programa)
+
+    ;;Expresion
+    
+    (expresion (numero)   numero-lit)
+    
+    (expresion (identificador)   id-exp)
+
+    (expresion ("\""texto"\"")   texto-lit)
+    
+    (expresion ("("expresion primitiva-binaria expresion")")   primapp-bin-exp)
+       
+    (expresion (primitiva-unaria "(" expresion ")")   primapp-un-exp)
+
+    (expresion ("Si" expresion "entonces" expresion "sino" expresion "finSI") condicional-exp)
+
+    (expresion ("declarar" "(" (separated-list identificador "=" expresion ";") ")" "{" expresion "}" ) variableLocal-exp)
+
+    (expresion ("procedimiento" "(" (separated-list identificador ",") ")" "haga" expresion "finProc" ) procedimiento-ex)
+
+    (expresion ( "evaluar"  expresion "("(separated-list expresion ",") ")" "finEval") app-exp)
+
+    (expresion ("letrec" (arbno identificador "(" (separated-list identificador ",") ")" "=" expresion)  "in" expresion) letrec-exp)
+
+    (expresion ("var" "{" (arbno identificador = expresion ";") "}" "in" expresion) var-exp)
+    
+    (expresion ("cons" "{" (arbno identificador = expresion ";") "}" "in" expresion) cons-exp)
+
+    
+   
+  )
+)
+
+
